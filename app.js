ed2fa415e6cb35c8c8294cffeb9340954bbc1859
Dwij13 <dwijjindal2005@gmail.com>
2023-11-29 18:01:21 +0530

    complete

---
+++ app.js
@@ -0,0 +1,91 @@
+let randomImg = document.getElementById("special")
+let randomName =document.getElementById("specialName")
+let randomCat = document.getElementById("specialCategory")
+let gridBox = document.getElementById("gridBox")
+let btn = document.getElementById("tap")
+let modal = document.getElementById("modal")
+let order = document.getElementById("order")
+
+
+randomImg.addEventListener("click",()=>{
+    modal.style.display="flex"
+})
+modal.addEventListener("click",()=>{
+    modal.style.display="none"
+})
+btn.addEventListener("click",()=>{
+    gridBox.innerHTML="";
+    let category = document.getElementById("category").value;
+    getCategory(category);
+})
+
+
+
+
+
+
+async function getRandom(){
+    try{
+        let randomUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
+        let response = await fetch(randomUrl);
+        let data = await response.json();
+         let allItems = data.meals[0]
+         console.log(allItems)
+         console.log()
+        for(let i=1;i<=20;i++){
+            if(allItems[`strIngredient${i}`]!=""){
+            let list = document.createElement("li")
+            list.innerHTML= allItems[`strIngredient${i}`]
+            order.append(list);
+            }
+        }    
+        console.log(order)  
+        let imageLink = data.meals[0].strMealThumb;
+        let imageName = data.meals[0].strMeal
+        let imageCategory = data.meals[0].strCategory
+        randomImg.style.backgroundImage=`url(${imageLink})`
+        randomName.textContent=imageName;
+        randomCat.textContent=imageCategory;
+    }catch(err){
+        console.log(err)
+    }
+}
+getRandom()
+
+
+async function getCategory(category){
+    try{
+        let object =[];
+        let catUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+        let generate = await fetch(catUrl)
+        let food = await generate.json();
+        
+        object=food.meals;
+        console.log(object)
+        for(let i=0;i<object.length;i++){
+            let item = document.createElement("div")
+            item.className="item"
+            let div1 = document.createElement("div")
+            let img = document.createElement("img")
+            img.className="image"
+            div1.append(img)
+            let div2 = document.createElement("div")
+            let para = document.createElement("p")
+            div2.append(para)
+            item.append(div1,div2)
+            gridBox.append(item)
+            let catImage = object[i].strMealThumb
+            img.src=catImage
+            console.log(catImage)
+            let catName = object[i].strMeal
+            console.log(catName)
+            para.innerHTML=catName
+
+        }
+        
+
+    }catch{
+
+    }
+}
+getCategory()
